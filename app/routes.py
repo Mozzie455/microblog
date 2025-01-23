6f629ebf0126042b0b1dcf88c1c1fca81b1d4765
mozzie <joshuamutiso455@gmail.com>
2025-01-23 23:58:42 +0300

    login_required decorator

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,17 @@ from app import app
 from app.forms import LoginForm
 
 
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from flask_login import login_user
+from flask_login import login_required
+
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username':'Joshua'}
     posts = [
@@ -21,8 +30,21 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-         return redirect(url_for('/index'))
+         user = db.session.scalar(
+             sa.select(User).where(User.username == form.username.data))
+         if user is None or not user.check_password(form.password.data):
+             flash('Invalid username or password')
+             return redirect(url_for('login'))
+         login_user(user, remember=form.remember_me.data)
+         return redirect(url_for('index'))
 
-    return render_template('login.html', title='Sign in', form=form)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    login_user()
+    return redirect(url_for('index'))
